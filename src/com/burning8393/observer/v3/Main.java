572525f7ca305bcfeb01f399bc5ceb1d777dd450
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-21 22:06:45 +0800

    观察者模式第3版 加入观察者

---
+++ src/com/burning8393/observer/v3/Main.java
@@ -0,0 +1,32 @@
+package com.burning8393.observer.v3;
+
+/**
+ * 加入观察者
+ */
+class Child {
+    private boolean cry = false;
+    private Dad d = new Dad();
+
+    public boolean isCry() {
+        return cry;
+    }
+
+    public void wakeUp() {
+        cry = true;
+        d.feed();
+    }
+}
+
+class Dad {
+    public void feed() {
+        System.out.println("dad feeding...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        // do sth
+        child.wakeUp();
+    }
+}
