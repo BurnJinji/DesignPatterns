e093e9ebb22285f1fc5ce43ee6a041fbc4af2ac7
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-22 11:10:26 +0800

    代理模式v4 在对坦克代理的类代码中直接添加记录时间的代码

---
+++ src/com/burning8393/proxy/v4/Tank.java
@@ -0,0 +1,47 @@
+package com.burning8393.proxy.v4;
+
+import java.util.Random;
+
+/**
+ * 问题：记录坦克的移动时间
+ * 最简单的办法：修改代码，记录时间
+ * 问题2：如果无法改变方法源码呢？ benchmark
+ * 用继承?
+ * 使用代理
+ */
+public class Tank implements Moveable {
+
+    @Override
+    public void move() {
+        System.out.println("tank kalakala...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TankTimeProxy(new Tank()).move();
+    }
+}
+
+class TankTimeProxy implements Moveable {
+    Tank tank;
+
+    public TankTimeProxy(Tank tank) {
+        this.tank = tank;
+    }
+
+    @Override
+    public void move() {
+        long start = System.currentTimeMillis();
+        tank.move();
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
+
+interface Moveable {
+    void move();
+}
