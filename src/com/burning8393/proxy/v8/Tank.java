48135600068815814957486de0b53f73b361cc93
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-22 11:49:29 +0800

    代理模式v8 使用jdk的动态代理实现 横切代码和业务逻辑代码分离

---
+++ src/com/burning8393/proxy/v8/Tank.java
@@ -0,0 +1,71 @@
+package com.burning8393.proxy.v8;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Random;
+
+/**
+ * 问题：记录坦克的移动时间
+ * 最简单的办法：修改代码，记录时间
+ * 问题2：如果无法改变方法源码呢？ benchmark
+ * 用继承?
+ * 使用代理
+ * 代理有各种类型
+ * 问题：如何实现代理的各种组合？ 继承？ Decorator？
+ * 代理的对象改成Movable类型 越来越像Decorator
+ * 如果有stop方法需要代理
+ * 如果想让LogProxy可以重用，不仅可以代理Tank，还可以代理任何其他可以代理的类型 Object
+ * 毕竟日志记录，时间计算是很多方法都需要的东西 这时该怎么办？
+ * 分离代理行为与被代理对象
+ * 使用jdk的动态代理
+ *
+ * 横切代码与业务逻辑代码分离 AOP
+ */
+public class Tank implements Movable {
+
+    @Override
+    public void move() {
+        System.out.println("tank kalakala...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        Movable m = (Movable) Proxy.newProxyInstance(Tank.class.getClassLoader(), new Class[]{Movable.class}, new TimeProxy(tank));
+        m.move();
+    }
+}
+
+class TimeProxy implements InvocationHandler {
+
+    Movable m;
+
+    public TimeProxy(Movable m) {
+        this.m = m;
+    }
+
+    public void before() {
+        System.out.println("method start...");
+    }
+
+    public void after() {
+        System.out.println("method end!");
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        before();
+        Object o = method.invoke(m, args);
+        after();
+        return o;
+    }
+}
+
+interface Movable {
+    void move();
+}
